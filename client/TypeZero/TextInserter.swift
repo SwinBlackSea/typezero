2e7d761d5b8e66eb22511b3e57363c8f76db7f5c
Ubuntu <ubuntu@localhost.localdomain>
2026-08-02 18:48:24 +0800

    fix: copy text before accessibility paste

--- client/TypeZero/TextInserter.swift
+++ client/TypeZero/TextInserter.swift
@@ -8,29 +8,9 @@ enum InsertionResult {
 
 struct TextInserter {
     func insert(_ text: String) -> InsertionResult {
-        let systemWide = AXUIElementCreateSystemWide()
-        var focusedValue: CFTypeRef?
-        let focusedStatus = AXUIElementCopyAttributeValue(
-            systemWide,
-            kAXFocusedUIElementAttribute as CFString,
-            &focusedValue
-        )
-
-        if focusedStatus == .success, let focusedValue {
-            let focused = focusedValue as! AXUIElement
-            let setStatus = AXUIElementSetAttributeValue(
-                focused,
-                kAXSelectedTextAttribute as CFString,
-                text as CFTypeRef
-            )
-            if setStatus == .success {
-                return .inserted
-            }
-        }
-
         copyToPasteboard(text)
-        simulatePasteIfTrusted()
-        return .copied
+        guard simulatePasteIfTrusted() else { return .copied }
+        return .inserted
     }
 
     private func copyToPasteboard(_ text: String) {
@@ -39,13 +19,14 @@ struct TextInserter {
         pasteboard.setString(text, forType: .string)
     }
 
-    private func simulatePasteIfTrusted() {
+    private func simulatePasteIfTrusted() -> Bool {
         guard AXIsProcessTrusted(),
               let keyDown = CGEvent(keyboardEventSource: nil, virtualKey: 9, keyDown: true),
-              let keyUp = CGEvent(keyboardEventSource: nil, virtualKey: 9, keyDown: false) else { return }
+              let keyUp = CGEvent(keyboardEventSource: nil, virtualKey: 9, keyDown: false) else { return false }
         keyDown.flags = .maskCommand
         keyUp.flags = .maskCommand
         keyDown.post(tap: .cghidEventTap)
         keyUp.post(tap: .cghidEventTap)
+        return true
     }
 }
